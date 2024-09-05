de007bea32d8777b076bc67cd46eb51b7b626f5d
Lloyd <Lloydabonds@gmail.com>
2024-09-05 01:03:03 -0400

    okay break time

--- src/ProjectModal.jsx
+++ src/ProjectModal.jsx
@@ -1,35 +1,43 @@
 import { useState } from "react";
 
 export function ProjectModal({ featuredProjectsArray, toggle, toggleModal }) {
-  const [currentTab, setCurrentTab] = useState(featuredProjectsArray[0])
+  const [currentTab, setCurrentTab] = useState(featuredProjectsArray[0]);
 
   console.log(currentTab.name);
 
   return (
     <>
-      <div className={toggle ? "hello": "modalToggledOff" }>
+      <div className={toggle ? "hello" : "modalToggledOff"}>
         <div className="modal-viewport" onClick={toggleModal}></div>
 
-      <div className="modal-box">
-        <div className="project-modal-heading">@featuredprojects</div>
-        <div className="project-modal-top4">
-            <div className="pic"></div>
+        <div className="modal-box">
+          <div className="project-modal-heading">@featuredprojects</div>
+          <div className="project-modal-top4">
+            <div className="pic">
+              <img className="stockimage" src={currentTab.screencap} alt="" />
+            </div>
             <p className="text">{currentTab.body}</p>
           </div>
           <div className="tabs">
             {featuredProjectsArray.map((e) => {
               return (
-                <div onClick={() => { setCurrentTab(e) }}
+                <div
+                  onClick={() => {
+                    setCurrentTab(e);
+                  }}
                   className="tabBox"
-                  key={e.id}>{e.id}
-                </div>  
-              )
+                  key={e.id}
+                >
+                  {e.id}
+                </div>
+              );
             })}
           </div>
+          <div className="names">{currentTab.name}</div>
+          <div className="tech">tech icons</div>
+          <div className="more">{currentTab.more}</div>
         </div>
       </div>
     </>
   );
-
 }
-
